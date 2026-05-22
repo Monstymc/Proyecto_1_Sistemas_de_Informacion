06d9721b73671bc126611f922c484faa4a303912
ElianVivar <elian.vivar@epn.edu.ec>
2026-05-22 12:40:57 -0500

    Add files via upload

---
+++ proyecyoEv/src/proyecyoEv2/Carrito.java
@@ -0,0 +1,53 @@
+package proyecyoEv2;
+
+import java.util.ArrayList;
+
+public class Carrito {
+
+ private ArrayList<Producto> carrito;
+
+ public Carrito() {
+     carrito = new ArrayList<>();
+ }
+
+ public void agregarProducto(Producto producto) {
+     carrito.add(producto);
+     System.out.println("\nProducto agregado al carrito.");
+ }
+
+ public void mostrarCarrito() {
+
+     if (carrito.isEmpty()) {
+         System.out.println("Carrito vacio.");
+         return;
+     }
+
+     double total = 0;
+
+     System.out.println(" TU CARRITO DE VIRVUTO\n");
+
+     for (int i = 0; i < carrito.size(); i++) {
+         System.out.println((i + 1) + ". " + carrito.get(i));
+         total += carrito.get(i).getPrecio();
+     }
+
+     System.out.println("\nTotal: $" + String.format("%.2f", total));
+ }
+
+ public void eliminarProducto(int posicion) {
+     carrito.remove(posicion);
+     System.out.println("\nProducto eliminado.");
+ }
+
+ public int cantidadProductos() {
+     return carrito.size();
+ }
+
+ public Producto obtenerProducto(int indice) {
+     return carrito.get(indice);
+ }
+
+ public void vaciarCarrito() {
+     carrito.clear();
+ }
+}
